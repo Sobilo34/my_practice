8df573faba59c98db5f7bc1c475030ce8297cc76
Sobilo34 <bilalsolih60@gmail.com>
2024-04-25 10:02:53 +0100

    commit

--- Python/Day_03_100DaysOfCode.py
+++ Python/Day_03_100DaysOfCode.py
@@ -12,17 +12,20 @@ Obesed and Clinically Obesed with their BMI
 height = float(input("What is your height? ")) # Recieves the height
 weight = int(input("What is your weight? ")) # Recieves the width
 
-bmi = height / weight ** 2 # Calculates the BMI
+weight = weight * 1000
+bmi = weight / (height ** 2) # Calculates the BMI
+
 if bmi < 20:
+    print(bmi);
     print("You're Underweight :)\n") # BMI below 20 is Underweight
-
 elif bmi < 30:
+    print(bmi);
     print("You have normal weight :)\n") # BMI < 30 but > 20 is Normal
-
 elif bmi < 40:
+    print(bmi);
     print("You are obesed ):\n") # BMI < 40 but > 30 is obesed
-
 else:
+    print(bmi);
     print("You're clinically obesed ):\n") # BMI more that 40 is Clinically obesed
 
 
